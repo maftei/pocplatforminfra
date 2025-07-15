9d0a9cac2c9227e80f698265570b06ba689a5c4d
Razvan Maftei <razvanmaftei@Razvans-MacBook-Pro.local>
2025-07-15 14:24:57 +0300

    Trigger CI: test terraform plan

---
+++ main.tf
@@ -0,0 +1,4 @@
+module "ci_iam_user" {
+  source = "./modules/iam"
+  user_name = "tpocplatform-ci-user"
+}
---
+++ modules/iam/iam.tf
@@ -0,0 +1,17 @@
+resource "aws_iam_user" "ci_user" {
+  name = var.user_name
+}
+
+resource "aws_iam_access_key" "ci_user_key" {
+  user = aws_iam_user.ci_user.name
+}
+
+data "local_file" "policy_json" {
+  filename = "${path.module}/policy.json"
+}
+
+resource "aws_iam_user_policy" "ci_user_policy" {
+  name   = "${var.user_name}-policy"
+  user   = aws_iam_user.ci_user.name
+  policy = data.local_file.policy_json.content
+}
