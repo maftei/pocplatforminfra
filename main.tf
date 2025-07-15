13ef1ef473f2557938a232602602f10b5ec7480c
Razvan Maftei <razvanmaftei@Razvans-MacBook-Pro.local>
2025-07-15 14:30:47 +0300

    modifiedn json policy

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-module "ci_iam_user" {
-  source    = "./modules/iam"
-  user_name = "tpocplatform-ci-user"
-}
+# module "ci_iam_user" {
+#   source    = "./modules/iam"
+#   user_name = "tpocplatform-ci-user"
+# }
