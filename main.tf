9e0abb3eccdfe3d2bfe4901e2699afedf506fb45
Razvan Maftei <razvanmaftei@Razvans-MacBook-Pro.local>
2025-07-19 14:48:26 +0300

    added new user for helm package to ECR

--- main.tf
+++ main.tf
@@ -23,11 +23,11 @@ module "helm_charts_ecr" {
 }
 
 module "iam_ci_user" {
-  source     = "./modules/iam"
-  user_name  = "tpocplatform-ci-user"
+  source    = "./modules/iam"
+  user_name = "tpocplatform-ci-user"
 }
 
 module "iam_helm_user" {
-  source     = "./modules/iam"
-  user_name  = "tpocplatform-helm-user"
+  source    = "./modules/iam"
+  user_name = "tpocplatform-helm-user"
 }
